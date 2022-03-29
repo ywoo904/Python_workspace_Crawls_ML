c6ec8cfc3c31600bcc509f1ffc1d87a4da660925
Young Woo <95726798+ywoo904@users.noreply.github.com>
2022-03-28 21:59:46 -0400

    Python_Function
    Python_Function

--- Day09/day09.py
+++ Day09/day09.py
@@ -33,4 +33,70 @@ print(any(lst3)) #False
 print(any(lst4)) #True 
 
 # 4.bin(number) -> 10진수 정수를 2진수로 변환, 표현식 0b
-print(bin(234))
+# oct(number) -> 0o, hex(number)->0x 
+
+# 5. dir(x): 객체 x에서 제공하는 변수,내장함수,내장클래스 목록을 반환
+print(dir(lst3))
+
+# 6. eval(expr): 문자열 수식을 인수로 받아서 계산 가능한 파이썬 수식으로 변환 
+print(eval("10+20")+30)  #30
+# print(eval(10+"20+30")) #문자열이 아니기 떄문에 계산불가
+ 
+# 7.ord(character): 문자열을 인코딩값으로 반환 
+# ' ' => 0x20(32) 
+# 0 => 0x30(48)
+# A => 0x41(65)
+# a => 0x61(97)
+
+print(ord('0')) #0x30(48) 
+print(ord('a')) #97 
+print(ord('A')) #65 
+print(hex(ord('가'))) #0xac00(16진수) 
+
+# 8. chr(number): 인코딩값을 다시 문자열로 반환 
+print(chr(0x30)) #0 
+print(chr(0xac00)) #'가'
+
+#pow(x,y): x에 대한 y의 제곱을 계산하여 반환 
+print(pow(2,3)) #8 
+print(pow(10,3)) #1000
+print(pow(10,-1)) #0.1 
+print(pow(-8,-2)) #0.015625 
+
+#10. round(number,자리수): 올림값 처리  
+print(round(3.14159)) #3e
+print(round(3.14159))
+
+# 11. sorted(iterable): 반복가능한 원소들을 오름차순 또는 내림차순 정렬결과 반환  
+
+# 12. zip(iterable*): 반복가능한 객체와 객체간의 원소들을 묶어서 튜플로 반환 
+zi= zip([1,3,5],[2,4,6])
+print(list(zi)) #[(1, 2), (3, 4), (5, 6)]
+
+#python의 모듈 및 패키지 사용 
+# 모듈(Module)이란? 함수, 변수, 클래스들을 모아놓은 파일  
+# (모듈은 만들어놓은 다른 파이썬 프로그램에서 불러와 사용이 가능함) 
+# 모듈을 만드는 방법: *.py 확장자를 이용하여 만들 수 있음
+# 모듈을 불러오는 방법: import 명령어를 사용하여 모듈을 불러와 사용
+# (표준 라이브러리를 불러와 사용할 때에는 import 사용함)  
+
+#시간관련 모듈
+import datetime,time #(가공된것/순수날것)
+
+#datetime 모듈은 시간에 대하여 가공된 정보를 처리
+a= datetime.datetime.now() #현재시간
+print(a) #2022-03-28 21:43:28.347264 
+
+import datetime,time
+t= time.localtime()  
+s= time.localtime().tm_zone  
+print(t) #time.struct_time(tm_year=2022, tm_mon=3, tm_mday=28, tm_hour=21, tm_min=46, tm_sec=59, tm_wday=0, tm_yday=87, tm_isdst=1)
+print(s) #EDT 
+print(t.tm_year) #2022
+print(t.tm_mday) #28 
+
+start = time.time()  
+print(start) #1970.01.01.00시를 기점으로 'How many seconds?' 
+time.sleep(5) #5초간 작업하던 프로세스 Stop
+end = time.time() 
+print(end)
--- Day09/tempCodeRunnerFile.py
+++ Day09/tempCodeRunnerFile.py
@@ -1,2 +1,14 @@
-# 4.bin(number) -> 10진수 정수를 2진수로 변환 
-print(bin(234))
+
+import datetime,time
+t= time.localtime()  
+s= time.localtime().tm_zone  
+print(t) #time.struct_time(tm_year=2022, tm_mon=3, tm_mday=28, tm_hour=21, tm_min=46, tm_sec=59, tm_wday=0, tm_yday=87, tm_isdst=1)
+print(s) #EDT 
+print(t.tm_year) #2022
+print(t.tm_mday) #28 
+
+start = time.time()  
+print(start) #1970.01.01.00시를 기점으로 'How many seconds?' 
+time.sleep(5)
+end = time.time() 
+print(end)
